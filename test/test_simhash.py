96c383956dca75006449a0e70ee4fb68478786d7
Robert Easton <robert@ueni.com>
2018-07-21 11:26:42 +0100

    new test simple POST one text

--- test/test_simhash.py
+++ test/test_simhash.py
@@ -3,6 +3,8 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
 
+from simhash_app.calculate_simhash import hash_length
+
 class SimhashTest(TestCase):
 
     def setUp(self):
@@ -14,3 +16,16 @@ class SimhashTest(TestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.json(), {'count': 0, 'next': None, 'previous': None, 'results': []})
+
+    def test_post_hashes(self):
+        response = self.client.post(
+            '/hashes/', format='json',
+            data={
+                'guid': '1234', 'source': 'test',
+                'text': 'this is a moderate length text with enough words we can change to make it similar',
+            }
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.json()['bits_differ'], hash_length + 1)
+        self.assertEqual(response.json()['nearest_duplicate'], None)
